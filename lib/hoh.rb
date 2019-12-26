f5ba3d244f8e814ffa41c2cbfcbff3d1731e109b
Jin Wu <jinn.wuu@gmail.com>
2019-12-26 17:15:14 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -4,39 +4,57 @@
 #
 # The tests will guide your construction.
 #
- PORTION_1 = {
-   label: "Kingdom",
-   sub_category: {
-     label: "Phylum",
-     sub_category: {
-       label: "Class"
-     }
-   }
-  }
-
- PORTION_2 = {
-   label: "Order"
- }
-
- PORTION_3 = {
-   label: "Family",
-   sub_category: {
-     label: "Genus",
-   }
- }
-
- PORTION_4 = {
-   label: "Species",
-   sub_category: nil
- }
+# PORTION_1 = {
+#   label: "Kingdom",
+#   sub_category: {
+#     label: "Phylum",
+#     sub_category: {
+#       label: "Class"
+#     }
+#   }
+# }
+#
+# PORTION_2 = {
+#   label: "Order"
+# }
+#
+# PORTION_3 = {
+#   label: "Family",
+#   sub_category: {
+#     label: "Genus",
+#   }
+# }
+#
+# PORTION_4 = {
+#   label: "Species",
+#   sub_category: nil
+# }
 
 def naming_system
-  PORTION_1[:sub_category][:sub_category][:sub_category]=PORTION_2
-  PORTION_1[:sub_category][:sub_category][:sub_category][:sub_category]=PORTION_3
-  PORTION_1[:sub_category][:sub_category][:sub_category][:sub_category][:sub_category][:sub_category]=PORTION_4
-
-  PORTION_1
-
+  PORTION_1 = [
+    :label => "Kingdom",
+      :sub_category => {
+        :label => "Phylum",
+          :sub_category => {
+            :label => "Class",
+              :sub_category => {
+                :label => "Order",
+                  :sub_category => {
+                    :label = "Family",
+                      :sub_category => {
+                        :label = "Genus",
+                          :sub_category => {
+                            :label = "Species",
+                              :sub_category = {
+                                nil
+                              }
+                          }
+                      }
+                  }
+              }
+          }
+      }
+  ]
   # Remember:
   #  Kingdom
   #  Phylum
